3077ac5f45d5073096cd7c516552b2ba48102da9
Hoomam <hoaz1600@student.miun.se>
2024-04-30 22:33:21 +0200

    first commit

---
+++ src/js/auth.js
@@ -0,0 +1,81 @@
+"use strict"
+
+const url = "https://auth-production-afa2.up.railway.app/api/" 
+const addBtn = document.getElementById("add");
+//Event listener för logga in knappen
+addBtn.addEventListener("click", async (event)=>{
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    let error;
+    try {
+        const response = await fetch(url + "login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        });
+        
+        //If error
+        if(response.status == 400){
+            throw new Error("Invalid username/password");
+        }else if(response.status == 401){
+            throw new Error("Incorrect username/password");
+        }
+        const data = await response.json();
+        /* const token = data.response.token;  */
+        console.log(data);
+    } catch (err) {
+        error = err;
+        document.getElementById("printed").textContent = "Det gick inte att logga in pga: " + error.message;
+    }finally{
+        if(!error){
+            document.getElementById("printed").textContent = "Nu är du inloggad";
+        }
+    }
+});
+
+
+const signup = document.getElementById("signup");
+//Event listener för skapa konto knappen
+signup.addEventListener("click", async (event)=>{
+    event.preventDefault();
+
+    const newUsername = document.getElementById("newUsername").value;
+    const newPassword = document.getElementById("newPassword").value;
+    const email = document.getElementById("email").value; 
+
+    let account = {
+        username: newUsername,
+        password: newPassword,
+        email: email
+    }
+    let error;
+    try {
+        const response = await fetch(url + "register", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(account)
+        });
+       
+        //If error
+        if(response.status == 400){
+            throw new Error("Invalid Invalid input");
+        }else if(response.status == 500){
+            throw new Error("Internal server error");
+        }
+        
+    } catch (err) {
+        error = err;
+        document.getElementById("printed").textContent = "Det gick inte att skapa konto pga: " + error.message;
+    }finally{
+        if(!error){
+            document.getElementById("printed").textContent = "Ditt konto har skapats";
+        }
+    }
+    });
