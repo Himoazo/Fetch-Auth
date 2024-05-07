0d79c84ec60eca98e139c85fffa3dbac76b6e110
Hoomam <hoaz1600@student.miun.se>
2024-05-07 15:16:41 +0200

    Kontroller för login och sign up fält

--- src/js/auth.js
+++ src/js/auth.js
@@ -8,6 +8,10 @@ login.addEventListener("click", async (event)=>{
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    //Kontroll att fälten är ifyllda
+    if(!username || !password){
+        return document.getElementById("printed").textContent = "Felaktigt användarnamn/lösenord";
+    }
 
     let error;
     try {
@@ -50,10 +54,27 @@ const signup = document.getElementById("signup");
 signup.addEventListener("click", async (event)=>{
     event.preventDefault();
 
-    const newUsername = document.getElementById("newUsername").value;
-    const newPassword = document.getElementById("newPassword").value;
-    const email = document.getElementById("email").value; 
+    const newUsername = document.getElementById("newUsername").value.trim();
+    const newPassword = document.getElementById("newPassword").value.trim();
+    const email = document.getElementById("email").value.trim(); 
 
+    //Kontroll att fälten är korrekta
+    if(!newUsername || !newPassword || !email){
+        return document.getElementById("printed").textContent = "Alla fält måste vara ifyllda";
+    }
+    //Kontrollera email format
+    const emailRx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const correctFormat = emailRx.test(email);
+    if(correctFormat == false){
+        return document.getElementById("printed").textContent = "Email formatet måste vara xxx@xxx.xxx";
+    }
+    //Kontrollera att lösenordet är minst 6 char
+    const passRx = /^.{6,}$/;
+    const correctPass = passRx.test(newPassword);
+    if(correctPass == false){
+        return document.getElementById("printed").textContent = "Lösenordet måste minst innehålla 6 tecken";
+    }
+    
     let account = {
         username: newUsername,
         password: newPassword,
